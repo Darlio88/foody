e956b6af205050f562342199ffaf239b925bac80
darlio88 <danieldarlio@gmail.com>
2022-09-23 10:56:14 +0300

    new& final

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,7 +5,7 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import Pizza from '../assets/pizza.png'
 import { colors } from "../assets/colors";
 
-const Item = () =>{
+const Item = ({image, cost, name}) =>{
     return (
         <div 
         style={{height:320, width:240}}
@@ -16,16 +16,16 @@ const Item = () =>{
             right:"10px",
             height:"max-content", 
             position:"absolute"}}>
-            <img src={Pizza} alt='pizza' style={{width:240}}/>
+            <img src={image} alt='cost' style={{width:240}}/>
             </div>
             <div 
             style={{width:"180px", height:"200px",
             background:' linear-gradient(330deg, rgba(255,252,242,1) 0%, rgba(210,202,201,1) 34%, rgba(184,175,175,1) 95%,#B8AFAF)'}}
             className="flex rounded-t-3xl rounded-2xl pb-4 mt-20 mx-6 flex-col items-center justify-end space-y-3 shadow-2xl">
-            <div className="space-y-1">
+            <div className="space-y-1 ">
                 <h6 
-                className='font-bold'
-                style={{color:colors.black}}>Pappardelle</h6>
+                className='font-bold text-center'
+                style={{color:colors.black}}>{name}</h6>
                 <small   className='font-thin'
                 style={{color:colors.black}}>With vegetable</small>
             </div>
@@ -33,7 +33,7 @@ const Item = () =>{
                 <h6
                 className='font-bold'
                 style={{color:colors.black}}
-                >$ 35.00</h6>
+                >$ {cost}</h6>
             </div>
             </div>
           
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -4,17 +4,23 @@ import React from 'react'
 
 //item component
 import Item from './Item'
+//images
+import Pizza from '../assets/pizza.png'
+import Salad from '../assets/salad.png'
+import Spagetti from '../assets/spagetti.png'
+import Pasta from '../assets/pasta.png'
+import Beef from '../assets/beef.png'
 
 const Items = () => {
   return (
     <div 
     className='md:flex mt-10 '
     style={{}}>
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
+        <Item image={Pizza} cost={24} name='Pizza'/>
+        <Item image={Spagetti} cost={43} name='Spagetti'/>
+        <Item image={Beef} cost={33} name='Beef'/>
+        <Item image={Pasta} cost={19} name='Pasta'/>
+        <Item image={Salad} cost={28}  name='Pappardelle'/>
     </div>
   )
 }
